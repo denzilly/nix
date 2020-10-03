264092ef40088d9f9573c532b769c53fad7d3358
f1fantasyprices <f1fantasyprices@gmail.com>
2020-10-03 12:31:29 +0200

    first test

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
-# This is a sample Python script.
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_dangerously_set_inner_html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+
+app.layout = html.Div([
+    html.Iframe(srcDoc="""<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div id="tradingview_0a7ea"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+  <script type="text/javascript">
+  new TradingView.widget(
+  {
+  "width": 980,
+  "height": 610,
+  "symbol": "NASDAQ:AAPL",
+  "interval": "D",
+  "timezone": "Etc/UTC",
+  "theme": "dark",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "allow_symbol_change": true,
+  "container_id": "tradingview_0a7ea"
+}
+  );
+  </script>
+</div>
+<!-- TradingView Widget END -->""", width="1020", height="650"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(name)
+    ),
+])
 
-# Press the green button in the gutter to run the script.
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run_server(debug=True)
